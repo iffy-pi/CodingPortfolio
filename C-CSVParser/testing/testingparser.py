c4e440aae8e6bc51a919f08f519fcc7af701a73c
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2022-07-26 17:50:11 -0400

    Modified testing script with tests run

--- C-CSVParser/testing/testingparser.py
+++ C-CSVParser/testing/testingparser.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import subprocess
+import csv
+import threading
 
 
 def run_process(cmd_args, stdin=None, stdout=None, cwd=None, shell=True, wait=False, communicate=False, timeout=None, error_level=None):
@@ -58,14 +60,129 @@ def run_process(cmd_args, stdin=None, stdout=None, cwd=None, shell=True, wait=Fa
 def run_c_parser( input_file_addr, output_file_addr ):
 	with open(output_file_addr, 'w') as output_file:
 		# we will write stdout to the file
-		run_process(['testparser.out', input_file_addr], stdout=output_file, wait=True)
+		run_process(['testparser.out', input_file_addr], stdout=output_file, wait=True, cwd=os.getcwd())
 
+def run_python_parser(input_file_addr, output_file_addr):
+	with open( input_file_addr, 'r' ) as csv_file:
+		python_csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+
+		with open(output_file_addr, 'w' ) as output_file:
+			output_file.write('[\n')
+			for row in python_csv_reader:
+				row_str = None
+				for cell in row:
+					if not row_str:
+						row_str = '["{}"'.format(cell)
+					else:
+						row_str = row_str + ', ' + '"{}"'.format(cell)
+
+				row_str += ']'
+
+				output_file.write(row_str+'\n')
+			output_file.write(']\n')
+
+def make_output_file_name(desc, is_c=False, is_python=False):
+	if is_c:
+		return os.path.join( "results", "{}_c_parser.txt".format(desc))
+
+	elif is_python:
+		return os.path.join( "results", "{}_py_parser.txt".format(desc))
+
+	else:
+		return None
+
+def run_test( input_csv_file, output_file_desc ):
+
+	c_output_file_name = make_output_file_name(output_file_desc, is_c=True)
+	python_output_file_name = make_output_file_name(output_file_desc, is_python=True)
+
+	py_thread = threading.Thread(target=run_python_parser, args=(input_csv_file, python_output_file_name))
+	c_thread = threading.Thread(target=run_c_parser, args=(input_csv_file, c_output_file_name))
+
+	py_thread.start()
+	c_thread.start()
+
+	py_thread.join()
+	c_thread.join()
+	
+
+def run_test_set():
+	test_count = 1
+
+	test_results = []
+	for file in os.listdir( 'inputs' ):
+		print('Running Test {}...'.format(test_count))
+		test_name = "{}_test_{}".format(os.path.splitext(file)[0], test_count)
+
+		input_csv_file = os.path.join( 'inputs', file)
+
+		run_test( input_csv_file, test_name )
+
+		c_output_file = make_output_file_name(test_name, is_c=True)
+		py_output_file = make_output_file_name(test_name, is_python=True)
+
+		a_test_result = {
+			'test': test_count,
+			'name': test_name,
+			'input_file': input_csv_file,
+			'c_output_file': c_output_file,
+			'py_output_file': py_output_file,
+			'missing_outputs': False,
+			'different': False
+		}
+
+		c_output_exists = os.path.exists(c_output_file)
+		py_output_exists = os.path.exists(py_output_file)
+
+		if not c_output_exists or not py_output_exists:
+			a_test_result['missing_outputs'] = True
+			test_results.append(a_test_result)
+			continue
+
+		with open(c_output_file, 'r') as c_output:
+			with open(py_output_file, 'r') as py_output:
+				a_test_result['different'] = ( str(c_output.read()) != str(py_output.read()) )
+
+
+		test_results.append(a_test_result)
+		test_count += 1
+
+	return test_results
+
+def print_test_results(test_results, only_failures=False):
+	
+	for test in test_results:
+		if ( not only_failures ) or ( only_failures and (test['missing_outputs'] or test['different'])):
+			print('Test: {}'.format(test['test']))
+			print('Input File: {}'.format(test['input_file']))
+			print('Status: {}'.format( 'FAIL' if (test['missing_outputs'] or test['different']) else 'PASS' ))
+			print('')
+			print('C Output: {}'.format(test['c_output_file']))
+			print('PY Output: {}'.format(test['py_output_file']))
+			print('')
+			if test['missing_outputs']:
+				if not os.path.exists(test['c_output_file']):
+					print('C Output does not exist!')
+				if not os.path.exists(test['py_output_file']):
+					print('PY Output does not exist!')
+
+			if test['different']:
+				print('Outputs have differening content!')
+
+			print('===============================================>')
 
 
 def main():
-	# 
+	# testing the result
+	#run_c_parser( "..\\addresses.csv", "test_output.txt" )
 
+	#run_python_parser( "..\\addresses.csv", "test_output.txt" )
 
+	#run_test( "..\\addresses.csv", "test_output.txt" )
+
+	test_results = run_test_set()
+
+	print_test_results(test_results, only_failures=True)
 
 
 if __name__ == "__main__":
