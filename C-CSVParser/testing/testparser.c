952ec768bc666d951f3196b39ea0e15c8ea05866
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2022-08-02 16:35:01 -0400

    Updated testing script

--- C-CSVParser/testing/testparser.c
+++ C-CSVParser/testing/testparser.c
@@ -9,7 +9,7 @@ int main( int argc, char *argv[],  char *envp[] ){
 	}
 	char * filename = argv[1];
 
-	struct csv_table *table = open_and_parse_file_to_csv_table(filename, ',', FALSE, FALSE);
+	struct csv_table *table = open_and_parse_file_to_csv_table(filename, ',', '"', FALSE, FALSE);
 
 	print_csv_table(table);
 
